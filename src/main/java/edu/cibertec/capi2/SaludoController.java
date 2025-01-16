67c51be17e44a35df9c8f31b75e06b1221ca819b
miguel <mallquilm@gmail.com>
2025-01-15 19:20:35 -0500

    fix para aplicacion mvc con v5.3.39

--- src/main/java/edu/cibertec/capi2/SaludoController.java
+++ src/main/java/edu/cibertec/capi2/SaludoController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class SaludoController {
     
-    @RequestMapping("saludo")
+    @RequestMapping("saludo.do")
     public String saludar(){
         return "saludo.jsp";
     }
